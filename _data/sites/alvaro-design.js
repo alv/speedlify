d2a90203f2bd09ab6b1ccbd0b91bd5eb424e39ee
Álvaro Castaño <alvaro@vivaestudio.com>
2022-10-01 22:54:52 +0200

    FEAT - Config and cleanup

---
+++ _data/sites/alvaro-design.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "alvaro.design", // optional, falls back to object key
+	description: "Álvaro Castaño's website",
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	options: {
+		frequency: 60 * 12, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://alvaro.design/"
+	]
+};
