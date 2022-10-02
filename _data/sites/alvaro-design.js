26a04712e0251ef27142b45413e02d157e5981ce
Álvaro Castaño <alvaro@vivaestudio.com>
2022-10-02 23:12:07 +0200

    FEAT - Less time between runs

--- _data/sites/alvaro-design.js
+++ _data/sites/alvaro-design.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "Álvaro Castaño's website",
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 12, // (in minutes), 23 hours
+		frequency: 60 * 3, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://alvaro.design/"
