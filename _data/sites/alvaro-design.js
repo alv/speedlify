1ad14ffc4faba6641640d96f85a4ca7b73c7ff1b
Álvaro Castaño <alvaro@vivaestudio.com>
2022-11-03 15:16:39 +0100

    FEAT - Higher test frequency

--- _data/sites/alvaro-design.js
+++ _data/sites/alvaro-design.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "alvaro.design", // optional, falls back to object key
 	description: "Álvaro Castaño's website",
 	options: {
-		frequency: 60 * 3, // (in minutes), 3 hours
+		frequency: 1, // (in minutes), 3 hours
 	},
 	urls: [
 		"https://alvaro.design/"
